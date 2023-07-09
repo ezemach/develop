95a9c9cf334235e9d8f2282b5230d0b909a9011f
Ezequiel <ezemazzucco@gmail.com>
2023-07-09 00:42:32 -0300

    2do commit

--- src/main/java/com/example/quintoimpacto/models/Course.java
+++ src/main/java/com/example/quintoimpacto/models/Course.java
@@ -1,2 +1,44 @@
-package com.example.quintoimpacto.models;public class Course {
+package com.example.quintoimpacto.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    private Long id;
+    private String name;
+    private String description;
+    @ElementCollection
+    private List<Shift> shifts = new ArrayList<>();
+    @OneToMany(mappedBy = "course", fetch = FetchType.EAGER)
+    private Set<Inscription> inscriptions = new HashSet<>();
+    public Course() {
+    }
+    public Course(String name, String description, List<Shift> shifts) {
+        this.name = name;
+        this.description = description;
+        this.shifts = shifts;
+    }
+    public void addInscription(Inscription inscription){
+        inscription.setCourse(this);
+        inscriptions.add(inscription);
+    }
+
+    /* GETTERS */
+    public Long getId() {return id;}
+    public String getName() {return name;}
+    public String getDescription() {return description;}
+    public List<Shift> getShifts() {return shifts;}
+    public Set<Inscription> getInscriptions() {return inscriptions;}
+
+    /* SETTERS */
+    public void setName(String name) {this.name = name;}
+    public void setDescription(String description) {this.description = description;}
+    public void setShifts(List<Shift> shifts) {this.shifts = shifts;}
+    public void setInscriptions(Set<Inscription> inscriptions) {this.inscriptions = inscriptions;}
 }
--- src/main/java/com/example/quintoimpacto/models/Inscription.java
+++ src/main/java/com/example/quintoimpacto/models/Inscription.java
@@ -1,2 +1,29 @@
-package com.example.quintoimpacto.models;public class Inscription {
+package com.example.quintoimpacto.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Inscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    private Long id;
+    private LocalDateTime dateTime;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Course course;
+    public Inscription() {
+    }
+
+    /* GETTERS */
+    public Long getId() {return id;}
+    public LocalDateTime getDateTime() {return dateTime;}
+    public User getUser() {return user;}
+    public Course getCourse() {return course;}
+
+    /* SETTERS */
+    public void setDateTime(LocalDateTime dateTime) {this.dateTime = dateTime;}
+    public void setUser(User user) {this.user = user;}
+    public void setCourse(Course course) {this.course = course;}
 }
