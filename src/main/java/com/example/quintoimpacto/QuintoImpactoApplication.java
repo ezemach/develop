253855bc3933fbf4fdc6acc9f299736f9e3e80c7
Ezequiel <ezemazzucco@gmail.com>
2023-07-09 00:31:21 -0300

    first commit

---
+++ src/main/java/com/example/quintoimpacto/QuintoImpactoApplication.java
@@ -0,0 +1,13 @@
+package com.example.quintoimpacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintoImpactoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuintoImpactoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/quintoimpacto/models/User.java
@@ -0,0 +1,2 @@
+package com.example.quintoimpacto.models;public class User {
+}
